45037706e78f3720529ec2c9608cbbb6e0c4f532
oladaolu <107776620+oladaolu@users.noreply.github.com>
2023-07-28 13:01:36 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,7 +26,14 @@ resource "aws_security_group" "instance_sg" {
     to_port = 80
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }
+   }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  
+   }
 }
 
 output "ec2_public_ip" {
