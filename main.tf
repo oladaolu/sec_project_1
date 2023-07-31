3720fe3b5de6f75b2862e33e0717d75f1aa19505
oladaolu <107776620+oladaolu@users.noreply.github.com>
2023-07-30 22:13:10 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,6 +13,32 @@ resource "aws_instance" "ec2_instance" {
   vpc_security_group_ids = [aws_security_group.instance_sg.id]
 }
 
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]  # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
 resource "aws_security_group" "instance_sg" {
   name_prefix = "Instance-SG-"
   
